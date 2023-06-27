ec6df609ebaff787f3a4b9197a56233d44ba24b2
Ryota <ryoenoki2013@gmail.com>
2023-06-27 10:28:06 +0900

    update: price

--- src/app/products/[handle]/page.js
+++ src/app/products/[handle]/page.js
@@ -90,8 +90,8 @@ export default async function Example({ params }) {
               type="button"
               className="w-full bg-gray-900 border border-transparent rounded-md py-3 px-8 flex items-center justify-center text-base font-medium text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-50 focus:ring-gray-500"
             >
-              {/* Pay {formatPrice(product.pricerange.minVariantPrice.amount)} */}
-              Pay Now
+              Pay{" "}
+              {formatPrice(productByHandle.priceRange.minVariantPrice.amount)}
             </button>
             <button
               type="button"
