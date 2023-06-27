3906de4605c32bae813598b7b4f07afaec60ae14
Ryota <ryoenoki2013@gmail.com>
2023-06-27 23:01:54 +0900

    add: checkoutMutation

--- src/app/products/[handle]/page.js
+++ src/app/products/[handle]/page.js
@@ -200,3 +200,15 @@ const SingleProductQuery = gql`
     }
   }
 `;
+
+const checkoutMutation = gql`
+  mutation CheckoutCreate($variantId: ID!) {
+    checkoutCreate(
+      input: { lineItems: { variantId: $variantId, quantity: 1 } }
+    ) {
+      checkout {
+        webUrl
+      }
+    }
+  }
+`;
